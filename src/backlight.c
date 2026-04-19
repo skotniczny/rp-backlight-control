ad5460f2e97619d6c14a9c0c07cdc8cbe9400ade
Łukasz <skotniczny@outlook.com>
2026-04-19 20:58:33 +0200

    refactor: load UI layout from GtkBuilder instead of building it in code

--- src/backlight.c
+++ src/backlight.c
@@ -60,32 +60,21 @@ int main (int argc, char *argv[])
   textdomain (GETTEXT_PACKAGE);
 
   gtk_init (&argc, &argv);
-  GtkWidget *win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  GtkWidget *box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
-  GtkWidget *lbl = gtk_label_new (_("Screen brightness"));
 
-  GtkWidget *slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 255, 1);
-  GtkWidget *btn_day = gtk_button_new_with_label (_("Day"));
-  GtkWidget *btn_night = gtk_button_new_with_label (_("Night"));
+  GtkBuilder *builder = gtk_builder_new_from_file (PACKAGE_DATA_DIR "ui/backlight.ui");
+  GObject *win = gtk_builder_get_object(builder, "main_window");
+  GObject *btn_day = gtk_builder_get_object(builder, "btn_day");
+  GObject *btn_night = gtk_builder_get_object(builder, "btn_night");
+  GObject *slider = gtk_builder_get_object (builder, "slider");
 
   gtk_range_set_value(GTK_RANGE(slider), get_actual_brightness());
-
-  gtk_scale_set_draw_value(GTK_SCALE(slider), TRUE);
-  gtk_scale_set_value_pos(GTK_SCALE(slider), GTK_POS_TOP);
 
   g_signal_connect (btn_day, "clicked", G_CALLBACK(on_day_clicked), slider);
   g_signal_connect (btn_night, "clicked", G_CALLBACK(on_night_clicked), slider);
   g_signal_connect (slider, "value-changed", G_CALLBACK(on_slider_changed), NULL);
   g_signal_connect (win, "delete_event", G_CALLBACK(end_program), NULL);
 
-  gtk_box_pack_start (GTK_BOX (box), lbl, TRUE, TRUE, 0);
-  gtk_box_pack_start (GTK_BOX (box), slider, TRUE, TRUE, 0);
-  gtk_box_pack_start (GTK_BOX (box), btn_day, TRUE, TRUE, 0);
-  gtk_box_pack_start (GTK_BOX (box), btn_night, TRUE, TRUE, 0);
-  gtk_container_add (GTK_CONTAINER (win), box);
-  gtk_container_set_border_width (GTK_CONTAINER (win), 5);
-  gtk_widget_show_all (win);
-  gtk_widget_set_size_request (win, 250, 10);
+  gtk_widget_show_all (GTK_WIDGET(win));
   gtk_main ();
   return 0;
 }
